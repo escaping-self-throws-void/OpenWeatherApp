187b959a6ba06cd63e9ca89c1cefe448ecce09db
Paul Matar <paul_matar@icloud.com>
2022-06-19 11:40:07 +0300

    Commit #12 add unit tests

--- OpenWeatherAppTests/WeatherViewModelTests.swift
+++ OpenWeatherAppTests/WeatherViewModelTests.swift
@@ -6,31 +6,55 @@
 //
 
 import XCTest
+import CoreLocation
 @testable import OpenWeatherApp
 
-class OpenWeatherAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class OpenWeatherAppTests: XCTestCase, CLLocationManagerDelegate {
+    
+    var viewModel: WeatherViewModel?
+    
+    override func setUp() {
+        super.setUp()
+        viewModel = WeatherViewModel(locDelegate: self, closure: {})
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        viewModel = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testDateTimeCreateWithRandomUnix() throws {
+        let unix = Double.random(in: 300...600)
+        let result = viewModel?.createDateTime(unix: unix)
+        XCTAssertNotNil(result)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testFetchWeatherWithCities() async throws {
+        let citis = ["Rome", "Toronto", "Byblos"]
+        let list = try await viewModel?.fetchWeather(for: citis)
+        XCTAssertNotNil(list)
     }
-
+    
+    func testFetchWeatherFromGeoLocation() async throws {
+        let lon = Double.random(in: 1...50)
+        let lat = Double.random(in: 1...50)
+        
+        let data = try await viewModel?.fetchWeather(lat: lat, lon: lon)
+        XCTAssertNotNil(data)
+    }
+    
+    func testGetDescriptionFromList() async throws {
+        let citis = ["Rome", "Toronto", "Byblos"]
+        let list = try await viewModel?.fetchWeather(for: citis)
+        let description = viewModel?.getDescription((list?.first)!)
+        XCTAssertNotNil(description)
+    }
+    
+    func testGetImageFromList() async throws {
+        let citis = ["Rome", "Toronto", "Byblos"]
+        let list = try await viewModel?.fetchWeather(for: citis)
+        let strImage = viewModel?.getImage((list?.first)!)
+        let image = UIImage(systemName: strImage!)
+        XCTAssertNotNil(image)
+    }
 }
